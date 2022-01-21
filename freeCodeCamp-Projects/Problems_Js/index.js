2f6dd8b4eb4ba1ec5a26f6dd2b732b35d8c4fc8e
ichimgabi97 <ichimgabi97@gmail.com>
2022-01-21 16:05:06 +0200

    js

--- freeCodeCamp-Projects/Problems_Js/index.js
+++ freeCodeCamp-Projects/Problems_Js/index.js
@@ -163,3 +163,15 @@ class Thermostat {
   thermos.temperature = 26;
   temp = thermos.temperature; // 26 in Celsius
   console.log(temp);
+
+  //------------------------------------------------------------------------
+
+  //Remove Whitespace from Start and End
+
+let hello = "   Hello, World!  ";
+let wsRegex = /^\s*|\s*$/g; // Change this line
+let result = hello.replace(wsRegex, ''); // Change this line
+console.log(result);
+
+
+//---------------------------------------------------------------------------
